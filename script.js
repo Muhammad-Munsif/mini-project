c6bf9c7b8d9d9616a0132bbf2c7a6c07e62cb228
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-09-15 21:41:49 +0500

    small change

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const kidApp = (e) =>{
-    e.preventDefault()
-    const input = document.getElementById("letter")
-    const letter = input.value.toLowerCase()
-    const title = document.getElementById("title")
-    const picture = document.getElementById("picture")
+const kidApp = (e) => {
+  e.preventDefault();
+  const input = document.getElementById("letter");
+  const letter = input.value.toLowerCase();
+  const title = document.getElementById("title");
+  const picture = document.getElementById("picture");
 
-    if(letter == "a"){
-        title.innerHTML = "Apple";
-        picture.src = "images/images.jpg"
-    }
-    
-    else{
-        title.innerHTML = "option is not found";
-        picture.src = "images/no-image-icon-10.png"
-    }
+  if (letter == "a") {
+    title.innerHTML = "Apple";
+    picture.src = "images/images.jpg";
+  } else {
+    title.innerHTML = "option is not found";
+    picture.src = "images/no-image-icon-10.png";
+  }
+};
+function clearInput() {
+  document.getElementById("letter").value = "";
 }
